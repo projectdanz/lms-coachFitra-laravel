0d5e6da216080e9bcd35d736226a8248c18ba04e
iqbaljlldn <iqbaljalaludin1309@gmail.com>
2025-08-30 19:25:31 +0700

    change the course_id column to nullable

--- database/migrations/2025_08_30_103959_add_course_id_to_users.php
+++ database/migrations/2025_08_30_103959_add_course_id_to_users.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('course_id')->constrained('courses')->cascadeOnDelete();
+            $table->foreignId('course_id')->nullable()->constrained('courses')->cascadeOnDelete();
         });
     }
 
