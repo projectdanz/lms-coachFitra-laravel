db26fa59c7f9af411c133615d901d9f9dc6d45ff
iqbaljlldn <iqbaljalaludin1309@gmail.com>
2025-09-08 13:40:40 +0700

    defining scheduler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CronAuth;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -10,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-        api: __DIR__.'/../routes/api.php',
+        api: __DIR__ . '/../routes/api.php',
         apiPrefix: 'api/',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -23,4 +24,24 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(function () {
+            app(\App\Http\Controllers\PaymentController::class)->processPendingUsers(
+                new \Illuminate\Http\Request(['secret' => config('webhook.cron_secret')])
+            );
+        })->everyFiveMinutes();
+
+        $schedule->call(function () {
+            app(\App\Http\Controllers\PaymentController::class)->healthCheck(
+                new \Illuminate\Http\Request(['secret' => config('webhook.cron_secret')])
+            );
+        })->everyFifteenMinutes();
+
+        $schedule->call(function () {
+            app(\App\Http\Controllers\PaymentController::class)->cleanupOldRecords(
+                new \Illuminate\Http\Request(['secret' => config('webhook.cron_secret')])
+            );
+        })->dailyAt('02:00');
+    })
+    ->create();
